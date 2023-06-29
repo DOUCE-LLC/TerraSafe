0c4a1b95bed594388a22ff194f08e45ef06db2c3
DOUCE_LLC <matiasaguilarprogramacion@gmail.com>
2023-06-29 13:11:41 -0300

    13.11

---
+++ Web Scrapping/usgs.py
@@ -0,0 +1,5 @@
+# https://earthquake.usgs.gov/fdsnws/event/1/query?format=csv&starttime=1930-01-01&endtime=1930-12-31
+# si lo abris te descarga el csv.
+
+# https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=1930-01-01&endtime=1930-12-31
+# visualizar la api
